5f1847a6437719e627f4d2e627be590b22a37b25
dbrdx <d.kabardynskyi@gmail.com>
2022-09-19 21:25:00 +0200

    newdd

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import json
+import time
 print("Hello World")
 print("Hello GITHUBd")
+curr = "Hello"
+with open("sample.json", "w") as outfile:
+    json.dump(curr, outfile)
+
+while True:
+    time.sleep(1)
